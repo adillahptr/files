45289cca5146cef33db6b631e8320f58bb4f14ea
Adillah Putri <adllhputri@gmail.com>
2023-12-10 06:55:31 +0700

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ python manage.py collectstatic -c
 export DJANGO_SUPERUSER_PASSWORD=admin
 python manage.py createsuperuser --no-input --username=admin  --email=admin@mail.com
 
-python manage.py runserver 0.0.0.0:8080
+gunicorn takis.wsgi
